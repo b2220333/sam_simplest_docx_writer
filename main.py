1e450a14fabcd3e4462a184946be434f5dde8ce1
b2220333 <b2220333@gmail.com>
2019-11-19 17:59:48 +0800

    v0.2--列出所有大綱

--- main.py
+++ main.py
@@ -3,5 +3,9 @@
 
 #開啟docx（聽說不能開doc檔）
 from docx import Document
-Doc = Document('../linux應用_v9.75.doc')
-print(f'Doc={Doc}')
+doc1 = Document('../linux應用_v9.75.doc')
+#print(f'Doc={Doc}')
+
+for paragraph in doc1.paragraphs:
+    parStr = paragraph.text
+    print(f'parStr={parStr}')
