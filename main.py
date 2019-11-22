8054ce5412c3cebe26abfe45aee2107d38ff98f6
b2220333 <b2220333@gmail.com>
2019-11-22 16:01:12 +0800

    v0.6--首次能載入Linux文件讀取2個大綱階層

--- main.py
+++ main.py
@@ -8,9 +8,14 @@ doc1 = Document('../linux應用_v9.75.doc')
 
 #列出所有大綱
 #for paragraph in doc1.paragraphs:
+    #第1階會輸出：Heading 1
+    #第2階會輸出：Heading 2
+    #...
+    #依此類推
     #print(f'大綱階層為：{paragraph.style.name}')
     #print(f'內文為：{paragraph.text}')
 
+#exit(0)
 #建立Tkinter GUI
 import tkinter
 from tkinter import ttk
@@ -31,6 +36,7 @@ tree.column("欄位ID 2", width=100)
 tree.heading("欄位ID 1", text="欄位文字1")
 tree.heading("欄位ID 2", text="欄位文字2")
 
+"""
 #第一階層-1
 #第二欄位表順序（0表最優先）
 tree.insert("" , 0,'階層1-1 ID', text="階層1-1文字", values=['a','b'])
@@ -48,9 +54,43 @@ linux_img2 = linux_img2.resize((30,30), Image.ANTIALIAS)
 linux_img2 = ImageTk.PhotoImage(linux_img2)
 tree.insert(apple, 3,"階層2-1 ID2 pic2", text='linux.png', open=True, image=linux_img2,
                  value=['0','1'])
-
 #第一階層-3
 tree.insert('',3,"階層1-3 ID", text="階層1-3文字", values=['g','h'])
+
+"""
+
+#準備建立獨特的ID
+ID = 0
+
+#若N不變表示多了同階
+#若N變大表示降了一階（如階層一->階層二）
+#若N變小表示升了一階（如階層二->階層一）
+#取得所有大綱
+#先建立特殊的首個第一階層
+previous_level = 0
+var_level = None
+for paragraph in doc1.paragraphs:
+    #第1階會輸出：Heading 1
+    #第2階會輸出：Heading 2
+    #...
+    #依此類推
+    #print(f'大綱階層為：{paragraph.style.name}')
+    #print(f'內文為：{paragraph.text}')
+    #開始取得Heading N的數字N
+    #str[str.index(' ')+1:]
+    if previous_level == 0:
+        #表示目前大綱尚未建立任何內容
+        var_level = tree.insert("", ID, f'{ID}', text={paragraph.text}, values=['a', 'b'])
+        previous_level = 1
+        ID = 1
+    else:
+        #表示目前至少有一個大綱
+        now_level = int(paragraph.style.name[paragraph.style.name.index(' ')+1:])
+        if now_level > previous_level:
+            #準備降階
+            #tree.insert("", 0, '階層1-1 ID', text="階層1-1文字", values=['a', 'b'])
+            var_level = tree.insert(var_level, ID, f'{ID}', text={paragraph.text}, values=['a', 'b'])
+        break
 #tree.grid()
 tree.pack()
 root.mainloop()
